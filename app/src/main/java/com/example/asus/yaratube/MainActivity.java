9517037a48fc9e9253e3bacacc3285570d11c2c8
NaarGes <narges.sadeghi73@gmail.com>
2018-08-11 20:49:54 +0430

    add drawer navigation to main activity

--- app/src/main/java/com/example/asus/yaratube/MainActivity.java
+++ app/src/main/java/com/example/asus/yaratube/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.asus.yaratube;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // FIXME minimum sdk is 17, use another way
+        // change app direction to RTL
+        getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        // drawer settings
+        drawerLayout = findViewById(R.id.drawer);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout, R.string.Open, R.string.Close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+    }
+
+    // hamburger menu work
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return (actionBarDrawerToggle.onOptionsItemSelected(item)) || super.onOptionsItemSelected(item);
+    }
+
+    // close the drawer if opened when back button tapped
+    @Override
+    public void onBackPressed() {
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
