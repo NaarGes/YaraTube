6884e0a790cbc045fb8ec33d85724d59ea12838a
NaarGes <narges.sadeghi73@gmail.com>
2018-09-08 11:38:03 +0430

    Prevent screen from lock/dim while playing video

--- app/src/main/java/com/example/asus/yaratube/ui/player/PlayerActivity.java
+++ app/src/main/java/com/example/asus/yaratube/ui/player/PlayerActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import com.example.asus.yaratube.R;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
@@ -21,6 +22,7 @@ public class PlayerActivity extends AppCompatActivity {
 
     private String mediaSource;
     private SimpleExoPlayer player;
+    private PlayerView playerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,15 +44,16 @@ public class PlayerActivity extends AppCompatActivity {
 
         // 2. Create the player
         player = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+        player.addListener(new PlayerEventListener());
 
         // Bind the player to the view
-        PlayerView playerView = findViewById(R.id.player);
+        playerView = findViewById(R.id.player);
         playerView.setPlayer(player);
 
         // prepare player
         // Produces DataSource instances through which media data is loaded.
         DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
-                Util.getUserAgent(this, "YaraTube"), bandwidthMeter);
+                Util.getUserAgent(this, "YaraTube Player"), bandwidthMeter);
 
         // This is the MediaSource representing the media to be played.
         HlsMediaSource videoSource = new HlsMediaSource.Factory(dataSourceFactory).createMediaSource(Uri.parse(mediaSource));
@@ -63,5 +66,33 @@ public class PlayerActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         player.release();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        player.setPlayWhenReady(false);
+        player.getPlaybackState();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        player.setPlayWhenReady(true);
+        player.getPlaybackState();
+    }
+
+    private class PlayerEventListener extends Player.DefaultEventListener {
+        @Override
+        public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+            if (playbackState == Player.STATE_IDLE || playbackState == Player.STATE_ENDED ||
+                    !playWhenReady) {
+                playerView.setKeepScreenOn(false);
+            } else { // STATE_IDLE, STATE_ENDED
+                // This prevents the screen from getting dim/lock
+                playerView.setKeepScreenOn(true);
+            }
+        }
     }
 }
